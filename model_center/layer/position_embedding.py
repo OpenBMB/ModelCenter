c02dff14c5151914a1ae1a4b64fd8dbbd5c17464
Achazwl <323163497@qq.com>
2022-03-17 15:53:38 +0800

    fix gptj rotary dimension

--- model_center/layer/position_embedding.py
+++ model_center/layer/position_embedding.py
@@ -91,46 +91,45 @@ class RotaryEmbedding(torch.nn.Module):
         super().__init__()
         self.rotary_dim = rotary_dim
 
-    def fixed_pos_embedding(self, x, seq_dim=1, seq_len=None, dtype = torch.float):
+    def fixed_pos_embedding(self, x, seq_len=None, dtype = torch.float):
         dim = x.shape[-1]
-        if seq_len is None:
-            seq_len = x.shape[seq_dim]
         inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2) / dim))
         sinusoid_inp = torch.einsum("i , j -> i j", torch.arange(seq_len), inv_freq).to(x.device).to(dtype)
         return torch.sin(sinusoid_inp), torch.cos(sinusoid_inp)
 
     def rotate_every_two(self, x):
-        x1 = x[:, :, :, ::2]
-        x2 = x[:, :, :, 1::2]
+        x1 = x[:, :, ::2]
+        x2 = x[:, :, 1::2]
         x = torch.stack((-x2, x1), axis=-1)
         return x.flatten(-2)  # in einsum notation: rearrange(x, '... d j -> ... (d j)')
 
 
     def apply_rotary_pos_emb(self, x, sincos, offset=0):
-        sin, cos = map(lambda t: t[None, offset : x.shape[1] + offset, None, :].repeat_interleave(2, 3), sincos)
+        sin, cos = map(lambda t: t[None, offset : x.shape[1] + offset, :].repeat_interleave(2, 2), sincos)
         # einsum notation for lambda t: repeat(t[offset:x.shape[1]+offset,:], "n d -> () n () (d j)", j=2)
         return (x * cos) + (self.rotate_every_two(x) * sin)
 
     def forward(self, h_q, h_k):
         """
         Args:
-            h_q : (batch_size, len_q, num_heads, dim_head)
-            h_k : (batch_size, len_k, num_heads, dim_head)
-        Returns:
-            h_q : (batch_size, len_q, num_heads, dim_head)
-            h_k : (batch_size, len_k, num_heads, dim_head)
-        """
-        k_rot = h_k[:, :, :, : self.rotary_dim]
-        k_pass = h_k[:, :, :, self.rotary_dim :]
+            h_q : (batch_size*num_head, len_q, dim_head)
+            h_k : (batch_size*num_head, len_k, dim_head)
 
-        q_rot = h_q[:, :, :, : self.rotary_dim]
-        q_pass = h_q[:, :, :, self.rotary_dim :]
+        Return:
+            h_q : (batch_size*num_head, len_q, dim_head)
+            h_k : (batch_size*num_head, len_k, dim_head)
+        """
+        q_rot = h_q[:, :, : self.rotary_dim]
+        q_pass = h_q[:, :, self.rotary_dim :]
+
+        k_rot = h_k[:, :, : self.rotary_dim]
+        k_pass = h_k[:, :, self.rotary_dim :]
 
         seq_len = h_k.shape[1]
-        sincos = self.fixed_pos_embedding(k_rot, 1, seq_len=seq_len, dtype=h_k.dtype)
+        sincos = self.fixed_pos_embedding(k_rot, seq_len=seq_len, dtype=h_k.dtype)
         k_rot = self.apply_rotary_pos_emb(k_rot, sincos, offset=0)
         q_rot = self.apply_rotary_pos_emb(q_rot, sincos, offset=0)
 
-        h_k = torch.cat([k_rot, k_pass], dim=-1)
         h_q = torch.cat([q_rot, q_pass], dim=-1)
-        return h_k, h_q
+        h_k = torch.cat([k_rot, k_pass], dim=-1)
+        return h_q, h_k
--- tests/test_gptj.py
+++ tests/test_gptj.py
@@ -18,18 +18,19 @@ def main():
 
     hug_gptj = hugGPTj.from_pretrained("EleutherAI/gpt-j-6B").cuda().eval().half()
     
-    batch = 1
-    max_encoder_length = 512
-    input_ids = torch.randint(config.vocab_size, (batch, max_encoder_length,), dtype=torch.int32).cuda()
-    length = torch.ones((batch, ), dtype=torch.int32).cuda() * max_encoder_length
-    attention_mask = torch.arange(input_ids.shape[1], device=input_ids.device)[None, :].repeat(input_ids.shape[0], 1) < length[:, None]
+    for _ in range(10):
+        batch = 1
+        max_encoder_length = 512
+        input_ids = torch.randint(config.vocab_size, (batch, max_encoder_length,), dtype=torch.int32).cuda()
+        length = torch.randint(max_encoder_length, (batch, ), dtype=torch.int32).cuda()
+        attention_mask = torch.arange(input_ids.shape[1], device=input_ids.device)[None, :].repeat(input_ids.shape[0], 1) < length[:, None]
 
-    bmt_logits = bmt_gptj(input_ids = input_ids, attention_mask = attention_mask, return_logits=True)
-    hug_logits = hug_gptj(input_ids = input_ids, attention_mask = attention_mask).logits
-    b = (bmt_logits*attention_mask[:,:,None])
-    h = hug_logits*attention_mask[:,:,None]
-    d = (h - b).abs()
-    print(d.max())
+        bmt_logits = bmt_gptj(input_ids = input_ids, attention_mask = attention_mask, return_logits=True)
+        hug_logits = hug_gptj(input_ids = input_ids, attention_mask = attention_mask).logits
+        b = (bmt_logits*attention_mask[:,:,None])
+        h = hug_logits*attention_mask[:,:,None]
+        d = (h - b).abs()
+        print(d.max())
 
 if __name__ == "__main__":
     main()
